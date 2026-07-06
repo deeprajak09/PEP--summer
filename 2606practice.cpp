fdcec25f7e9b8f8e141c8c78d0bb134459f0ef87
Deep Narayan Rajak <141905139+deeprajak09@users.noreply.github.com>
2026-07-06 10:08:52 +0530

    Add files via upload

---
+++ 2606practice.cpp
@@ -0,0 +1,31 @@
+//prefect number
+#include<iostream>
+using namespace std;
+
+int main(){
+     int n, sum=0;
+     cout<<"enter the number";
+     cin>>n;
+     for(int i = 1; i<=n/2; i++){
+        if(n%i==0){
+            sum += i;
+
+        }
+     }
+     if(sum == n)
+     cout<<n<<"is a perfect number";
+     else
+     cout<<n<<"is not a perfect number";
+}
+//
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, sum=0;
+    cout<<"enter the number";
+    cin>>n;
+    for(int i = 1;i<=n/2;i++){
+        
+    }
+}
